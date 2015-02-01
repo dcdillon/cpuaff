a2f57b9adaa5c538f0cee467b4454dd8d47b469c
Daniel C. Dillon <dcdillon@gmail.com>
2015-02-01 17:05:01 -0600

    working on get/set affinity for linux

--- include/cpuaff/impl/linux_impl/linux.hpp
+++ include/cpuaff/impl/linux_impl/linux.hpp
@@ -154,6 +154,7 @@ struct get_affinity
             {
                 if (CPU_ISSET(i, &cpu_set))
                 {
+                    std::cerr << "Affinity set to " << i << std::endl;
                     cpus.insert(cpu_identifier_wrapper(i));
                 }
             }
@@ -179,6 +180,7 @@ struct set_affinity
 
         for (; i != iend; ++i)
         {
+            std::cerr << "Setting affinity to " << i->get() << std::endl;
             CPU_SET(i->get(), &cpu_set);
         }
 
