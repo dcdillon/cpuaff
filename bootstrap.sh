26b3a222a79ad0f07a788dda9be791bfb43c6ee2
Daniel C. Dillon <dcdillon@gmail.com>
2017-10-28 16:15:48 -0500

    Made cleanup.sh and bootstrap.sh safer

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+pushd $DIR > /dev/null 2>&1
+
 aclocal
 autoconf
 autoheader
 automake --add-missing
+
+popd > /dev/null 2>&1
