ec54926c961754b4943cacfed7884a7ecda13745
Daniel C. Dillon <dcdillon@gmail.com>
2017-10-28 12:36:09 -0500

    Hardening tests and giving the option to make them weaker for travis-ci purposes.

--- tests/test.cpp
+++ tests/test.cpp
@@ -67,65 +67,146 @@ TEST_CASE("affinity_manager", "[affinity_manager]")
     {
         REQUIRE(manager.has_cpus());
 
-        cpuaff::cpu cpu;
-        cpuaff::cpu_set cpus;
+        cpuaff::cpu first_cpu;
 
-        REQUIRE(manager.get_cpu_from_index(cpu, 0));
-        REQUIRE(manager.get_cpu_from_id(cpu, cpu.id()));
-        REQUIRE(manager.get_cpu_from_id(cpu, cpu.id().get()));
-        REQUIRE(manager.get_cpu_from_spec(
-            cpu,
-            cpuaff::cpu_spec(cpu.socket(), cpu.core(), cpu.processing_unit())));
-
-        REQUIRE(manager.get_cpus(cpus));
-        REQUIRE(manager.get_cpus_by_numa(cpus, cpu.numa()));
-        REQUIRE(manager.get_cpus_by_socket(cpus, cpu.socket()));
-        REQUIRE(manager.get_cpus_by_core(cpus, cpu.core()));
-        REQUIRE(
-            manager.get_cpus_by_processing_unit(cpus, cpu.processing_unit()));
-        REQUIRE(manager.get_cpus_by_socket_and_core(cpus, cpu.socket(),
-                                                    cpu.core()));
-
-        REQUIRE(manager.get_affinity(cpus));
-        REQUIRE(cpus.size() > 0);
-
-        cpuaff::cpu_set new_affinity;
-        new_affinity.insert(cpu);
-        REQUIRE(manager.set_affinity(new_affinity));
-        REQUIRE(manager.get_affinity(cpus));
-
-        if (cpus.size() == new_affinity.size())
+        // We can get the first CPU by index
+        REQUIRE(manager.get_cpu_from_index(first_cpu, 0));
+        REQUIRE(first_cpu.socket() >= 0);
+        REQUIRE(first_cpu.core() >= 0);
+        REQUIRE(first_cpu.processing_unit() >= 0);
+        
+        // We can get this same CPU by its native handle wrapper
         {
+            cpuaff::cpu cpu;
+            REQUIRE(manager.get_cpu_from_id(cpu, first_cpu.id()));
+            REQUIRE(cpu == first_cpu);
+        }
+        
+        // We can get this came CPU by its raw native id
+        {
+            cpuaff::cpu cpu;
+            REQUIRE(manager.get_cpu_from_id(cpu, first_cpu.id().get()));
+            REQUIRE(cpu == first_cpu);
+        }
+        
+        // We can get this same CPU by its cpu_spec
+        {
+            cpuaff::cpu cpu;
+            REQUIRE(manager.get_cpu_from_spec(
+                cpu,
+                cpuaff::cpu_spec(first_cpu.socket(), first_cpu.core(), first_cpu.processing_unit())));
+        }
+
+        // We can get all the CPUs
+        {
+            cpuaff::cpu_set cpus;
+            REQUIRE(manager.get_cpus(cpus));
+            REQUIRE(!cpus.empty() > 0);
+        }
+        
+        // We can get all the CPUs for the NUMA of the first CPU
+        {
+            cpuaff::cpu_set cpus;
+            REQUIRE(manager.get_cpus_by_numa(cpus, first_cpu.numa()));
+            REQUIRE(!cpus.empty());
+        }
+        
+        // We can get all the CPUs for the socket of the first CPU
+        {
+            cpuaff::cpu_set cpus;
+            REQUIRE(manager.get_cpus_by_socket(cpus, first_cpu.socket()));
+            REQUIRE(!cpus.empty());
+        }
+        
+        // We can get all the CPUs with the same core as the first CPU
+        {
+            cpuaff::cpu_set cpus;
+            REQUIRE(manager.get_cpus_by_core(cpus, first_cpu.core()));
+            REQUIRE(!cpus.empty());
+        }
+        
+        // We can get all the CPUs with the same processing unit as the first
+        // CPU
+        {
+            cpuaff::cpu_set cpus;
+            REQUIRE(
+                manager.get_cpus_by_processing_unit(cpus, first_cpu.processing_unit()));
+            REQUIRE(!cpus.empty());
+        }
+        
+        // We can get all the CPUs on the same socket and core as the first cpu
+        {
+            cpuaff::cpu_set cpus;
+            REQUIRE(manager.get_cpus_by_socket_and_core(cpus, first_cpu.socket(),
+                                                        first_cpu.core()));
+            REQUIRE(!cpus.empty());
+        }
+
+        // We can get the affinity of the current thread
+        {
+            cpuaff::cpu_set cpus;
+            REQUIRE(manager.get_affinity(cpus));
+            REQUIRE(!cpus.empty());
+        }
+
+        // We can set the affinity of the current thread
+        {
+            cpuaff::cpu_set cpus;
+            cpuaff::cpu_set new_affinity;
+            
+            new_affinity.insert(first_cpu);
+            
+            REQUIRE(manager.set_affinity(new_affinity));
+            REQUIRE(manager.get_affinity(cpus));
+
+#if !defined(USING_TRAVIS_CI)
             cpuaff::cpu_set::iterator i = cpus.begin();
             cpuaff::cpu_set::iterator iend = cpus.end();
             cpuaff::cpu_set::iterator j = new_affinity.begin();
             cpuaff::cpu_set::iterator jend = new_affinity.end();
-
+    
             for (; i != iend && j != jend; ++i, ++j)
             {
                 REQUIRE((*i) == (*j));
             }
+#endif
         }
 
-        REQUIRE(manager.get_cpus(new_affinity));
-        REQUIRE(manager.set_affinity(new_affinity));
-        REQUIRE(manager.get_affinity(cpus));
-
-        if (cpus.size() == new_affinity.size())
+        // We can set the affinity of the current thread back to all cpus
         {
+            cpuaff::cpu_set cpus;
+            cpuaff::cpu_set new_affinity;
+            
+            REQUIRE(manager.get_cpus(new_affinity));
+            
+            REQUIRE(manager.set_affinity(new_affinity));
+            REQUIRE(manager.get_affinity(cpus));
+
+#if !defined(USING_TRAVIS_CI)
             cpuaff::cpu_set::iterator i = cpus.begin();
             cpuaff::cpu_set::iterator iend = cpus.end();
             cpuaff::cpu_set::iterator j = new_affinity.begin();
             cpuaff::cpu_set::iterator jend = new_affinity.end();
-
+    
             for (; i != iend && j != jend; ++i, ++j)
             {
                 REQUIRE((*i) == (*j));
             }
+#endif
         }
 
-        REQUIRE(manager.pin(cpu));
-        REQUIRE(manager.get_affinity(cpus));
+        // We can pin the affinity of the current thread to a particular CPU
+        {
+            cpuaff::cpu_set cpus;
+            
+            REQUIRE(manager.pin(first_cpu));
+            REQUIRE(manager.get_affinity(cpus));
+            
+#if !defined(USING_TRAVIS_CI)
+            REQUIRE(cpus.size() == 1);
+            REQUIRE(*cpus.begin() == first_cpu);
+#endif
+        }
     }
 }
 
@@ -140,48 +221,75 @@ TEST_CASE("affinity_stack", "[affinity_stack]")
         cpuaff::affinity_stack stack(manager);
 
         cpuaff::cpu_set original_affinity;
-        cpuaff::cpu_set new_affinity;
         cpuaff::cpu_set cpus;
 
+        // get the current affinity of this thread
         REQUIRE(stack.get_affinity(original_affinity));
-        REQUIRE(original_affinity.size() > 0);
+        REQUIRE(!original_affinity.empty());
+        
+        // push the current affinity of this thread onto the stack
         REQUIRE(stack.push_affinity());
+        
+        // verify that the affinities are the same (as we didn't change them)
         REQUIRE(stack.get_affinity(cpus));
         REQUIRE(cpus.size() == original_affinity.size());
 
-        if (cpus.size() == original_affinity.size())
+#if !defined(USING_TRAVIS_CI)
         {
             cpuaff::cpu_set::iterator i = cpus.begin();
             cpuaff::cpu_set::iterator iend = cpus.end();
             cpuaff::cpu_set::iterator j = original_affinity.begin();
             cpuaff::cpu_set::iterator jend = original_affinity.end();
-
+        
             for (; i != iend && j != jend; ++i, ++j)
             {
                 REQUIRE((*i) == (*j));
             }
         }
+#endif
 
+        cpuaff::cpu_set new_affinity;
         new_affinity.insert(*original_affinity.begin());
+        cpus.clear();
 
+        // set the thread's affinity to a single core
         REQUIRE(stack.set_affinity(new_affinity));
         REQUIRE(stack.get_affinity(cpus));
+        
+#if !defined(USING_TRAVIS_CI)
+        {
+            cpuaff::cpu_set::iterator i = cpus.begin();
+            cpuaff::cpu_set::iterator iend = cpus.end();
+            cpuaff::cpu_set::iterator j = new_affinity.begin();
+            cpuaff::cpu_set::iterator jend = new_affinity.end();
+    
+            for (; i != iend && j != jend; ++i, ++j)
+            {
+                REQUIRE((*i) == (*j));
+            }
+        }
+#endif
 
+        cpus.clear();
+        
+        // pop the affinity off the top of the stack and test that it is the
+        // same as the original affinity
         REQUIRE(stack.pop_affinity());
         REQUIRE(stack.get_affinity(cpus));
 
-        if (cpus.size() == original_affinity.size())
+#if !defined(USING_TRAVIS_CI)
         {
             cpuaff::cpu_set::iterator i = cpus.begin();
             cpuaff::cpu_set::iterator iend = cpus.end();
             cpuaff::cpu_set::iterator j = original_affinity.begin();
             cpuaff::cpu_set::iterator jend = original_affinity.end();
-
+    
             for (; i != iend && j != jend; ++i, ++j)
             {
                 REQUIRE((*i) == (*j));
             }
         }
+#endif
     }
 }
 
@@ -222,29 +330,29 @@ TEST_CASE("native_cpu_mapper", "[native_cpu_mapper]")
 {
     SECTION("native_cpu_mapper member functions")
     {
+#if !defined(USING_TRAVIS_CI)
         cpuaff::affinity_manager manager;
 
         REQUIRE(manager.has_cpus());
 
         cpuaff::native_cpu_mapper mapper;
 
-        if (mapper.initialize(manager))
+        REQUIRE(mapper.initialize(manager));
+        cpuaff::cpu_set cpus;
+        manager.get_cpus(cpus);
+
+        cpuaff::cpu_set::iterator i = cpus.begin();
+        cpuaff::cpu_set::iterator iend = cpus.end();
+
+        for (; i != iend; ++i)
         {
-            cpuaff::cpu_set cpus;
-            manager.get_cpus(cpus);
+            cpuaff::native_cpu_mapper::native_cpu_wrapper_type native;
+            cpuaff::cpu cpu;
 
-            cpuaff::cpu_set::iterator i = cpus.begin();
-            cpuaff::cpu_set::iterator iend = cpus.end();
-
-            for (; i != iend; ++i)
-            {
-                cpuaff::native_cpu_mapper::native_cpu_wrapper_type native;
-                cpuaff::cpu cpu;
-
-                REQUIRE(mapper.get_native_from_cpu(native, *i));
-                REQUIRE(mapper.get_cpu_from_native(cpu, native));
-                REQUIRE(mapper.get_cpu_from_native(cpu, native.get()));
-            }
+            REQUIRE(mapper.get_native_from_cpu(native, *i));
+            REQUIRE(mapper.get_cpu_from_native(cpu, native));
+            REQUIRE(mapper.get_cpu_from_native(cpu, native.get()));
         }
+#endif
     }
 }
